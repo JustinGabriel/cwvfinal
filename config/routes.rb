dd3bc68471ff26dc63423cf80b4bf960263f060b
Justin Gabriel <justin@campusweaver.com>
2013-03-12 09:37:49 -0500

    Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.routes.draw do
+RailsinstallerOsxDemo::Application.routes.draw do
     resources :users do
         member do
             get :following, :followers
@@ -17,6 +17,7 @@ SampleApp::Application.routes.draw do
     match '/help',    to: 'static_pages#help'
     match '/about',   to: 'static_pages#about'
     match '/contact', to: 'static_pages#contact'
+
     
     # The priority is based upon order of creation:
     # first created -> highest priority.
@@ -67,7 +68,7 @@ SampleApp::Application.routes.draw do
     
     # You can have the root of your site routed with "root"
     # just remember to delete public/index.html.
-    # root :to => 'welcome#index'
+    #root :to => 'home#index'
     
     # See how all your routes lay out with "rake routes"
     
